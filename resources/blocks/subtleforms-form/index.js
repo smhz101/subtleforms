1cb89675149b0a4cc5d257a1f0d85e1bac4b0b16
Muzammil Hussain <smhz101@gmail.com>
2026-05-11 06:57:29 +0500

    style: normalize registerBlockType call formatting

--- resources/blocks/subtleforms-form/index.js
+++ resources/blocks/subtleforms-form/index.js
@@ -3,8 +3,8 @@ import metadata from './block.json';
 import Edit from './edit.jsx';
 import save from './save.js';
 
-registerBlockType( metadata.name, {
+registerBlockType(metadata.name, {
 	...metadata,
 	edit: Edit,
 	save,
-} );
+});
